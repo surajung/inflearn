9d541a41dbc95df7c92ad594c396e4a4184dfa8a
surajung <surajung@saramin.co.kr>
2020-08-10 09:56:33 +0900

    vuex 적용

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
-    <TodoList v-bind:propsdata="todoItems"
-      v-on:removeItem="removeOneItem"
-      v-on:toggleItem="toggleOneItem"></TodoList>
-    <TodoFooter v-on:clearAll="clearAllItem"></TodoFooter>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
@@ -16,11 +14,6 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  data() {
-    return {
-      todoItems: []
-    }
-  },
   components: {
     TodoHeader,
     TodoInput,
@@ -28,35 +21,7 @@ export default {
     TodoFooter
   },
   methods: {
-    addOneItem(todoItem) {
-      const obj = {completed: false, item: todoItem};
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      this.todoItems.push(obj);
-    },
-    removeOneItem(todoItem, index) {
-      this.todoItems.splice(index, 1);
-      localStorage.removeItem(todoItem.item);
-    },
-    toggleOneItem(todoItem, index) {
-      // todoItem.completed = !todoItem.completed;  // data를 직접 변경하기 위해 아래와 같이 수정, 하위 컴퍼넌트와의 이벤트버스를 통해 data 변경은 지양
-      this.todoItems[index].completed = !this.todoItems[index].completed
-      localStorage.removeItem(this.todoItems[index].item);
-      localStorage.setItem(this.todoItems[index].item, JSON.stringify(this.todoItems[index]));
-    },
-    clearAllItem() {
-      localStorage.clear();
-      this.todoItems = [];
-    }
   },
-  created() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-  }
 }
 </script>
 
