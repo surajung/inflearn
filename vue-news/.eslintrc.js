7d7beda4c68102052eacc9307476aae5b1d18b14
surajung <hdoc79@gmail.com>
2020-08-30 10:48:44 +0900

    vue-news router 설정중

---
+++ vue-news/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "html",
+        "standard",
+        "vue"
+    ],
+    "rules": {
+    }
+};
